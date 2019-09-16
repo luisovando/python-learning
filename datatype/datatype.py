6a9ad49b625b6953d03104b4f4ff4cb6f3428f3e
Luis Ovando <isc.luisovando@gmail.com>
2019-09-16 18:44:26 -0500

    Check datatypes on Python

---
+++ datatype/datatype.py
@@ -0,0 +1,34 @@
+# Strings
+print("Hello World")
+print('Hello World')
+print('''Hello World''')
+print("""Hello World""")
+
+# type() function return datatype from argument
+print(type("Hello World"))
+
+# Numbers
+print(30) # nnt
+print(30.5) # float
+
+# Boolean
+True
+False
+
+# List -> In other languages is called as array
+[10, 20, 30]
+['A', 'B', 10, 30]
+[] # Define empty list
+
+# Tuples -> Is inmutable, used when data not change, this can be to different type, can't modify
+(10,20,30)
+() # Define empty tuple
+
+# Dictionaries
+{
+    "name": "Jorge",
+    "lastname":"Ramírez",
+    "nickname":"Boy"
+}
+
+None
