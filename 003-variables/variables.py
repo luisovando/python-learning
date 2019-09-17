22662d1b97c1d1ceda313df19ab92515182fe298
Luis Ovando <isc.luisovando@gmail.com>
2019-09-16 20:31:11 -0500

    Learn about variables, constants and literals

---
+++ 003-variables/variables.py
@@ -0,0 +1,25 @@
+# We will learn variables
+
+# This is a variable with snake_case
+batman_movie = "Batman Begins"
+
+# This is a variable with camelCase
+batmanMovie = "Batman: The Dark Knight"
+
+# This is a variable with PascalCase
+BatmanMovie = "Batman: The Dark Knight Rises"
+
+# All names are correct for Python interpeter, but... the name mmm... is the same object? NO!
+
+# It's ok? batman_movie = 300? Yes, because Python is not strong-typed language.
+
+# We can declare diferent variables on one line, check this!
+
+TLOR1, TLOR2, TLOR3 = "The Fellowship of the Ring", "The Two Towers", "The Return of the King"
+
+# Ok, but is possible print them on one shoot?, YES!
+
+print(TLOR1, TLOR2, TLOR3)
+
+# Constants
+PI = 3.1416
